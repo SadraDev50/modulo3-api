6e0b99729a8c83e8fc196783fcfe92ee698acbb7
SandraDev50 <sosandracosta@gmail.com>
2022-03-15 21:31:48 -0300

    Projeto Novelas Globais

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router =express.Router();
+
+router.get('/',function(req, res) {
+    res.status(200).send({
+        titulo:'Seja bem vindo consulte novelas',
+        data:'15/03/2022'
+    })
+
+})
+module.export = router   
